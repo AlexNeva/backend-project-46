e8e5348410ee931ecab590efdebc61f3ff39f4ed
Александр Невский <sashnevski@MacBook-Air-Aleksandr.local>
2024-06-18 19:17:15 +0300

    Add sorting for file

--- src/genDiff.js
+++ src/genDiff.js
@@ -1,5 +1,6 @@
 import path from 'node:path';
 import fs from 'node:fs';
+import sortBy from 'lodash/sortBy.js';
 
 const getPath = (filepath) => path.resolve(process.cwd(), filepath);
 const fileParser = (filepath) => JSON.parse(fs.readFileSync(filepath, 'utf8'));
@@ -10,7 +11,12 @@ const genDiff = (filepath1, filepath2) => {
 
   const parseredFile1 = fileParser(path1);
   const parseredFile2 = fileParser(path2);
-  console.log(parseredFile1);
+
+  const entries = Object.entries(parseredFile1);
+  const sortedEntries = sortBy(entries, 0);
+  const sortedFile1 = Object.fromEntries(sortedEntries);
+
+  console.log(sortedFile1);
   console.log(parseredFile2);
 };
 
