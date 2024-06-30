4766f314fe4200d48cc916671d97cf74e9e62adb
Александр Невский <sashnevski@MacBook-Air-Aleksandr.local>
2024-06-30 16:08:19 +0300

    Add yaml test

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -2,9 +2,20 @@ import fs from 'node:fs';
 import path from 'node:path';
 import genDiff from '../src/genDiff.js';
 
-test('diff compare', () => {
+let equal;
+
+beforeAll(() => {
+  equal = fs.readFileSync(path.resolve(process.cwd(), '__fixtures__/diff.txt'), 'utf8');
+});
+
+test('diff .json files', () => {
   const data = genDiff('__fixtures__/file1.json', '__fixtures__/file2.json');
-  const equal = fs.readFileSync(path.resolve(process.cwd(), '__fixtures__/diff.txt'), 'utf8');
+
+  expect(data).toEqual(equal);
+});
+
+test('diff .yaml files', () => {
+  const data = genDiff('__fixtures__/file1.yaml', '__fixtures__/file2.yaml');
 
   expect(data).toEqual(equal);
 });
