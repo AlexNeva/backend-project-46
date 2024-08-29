571bfc266da9ef87193da539d88c9f3ed7510d91
Александр Невский <sashnevski@MacBook-Air-Aleksandr.local>
2024-08-29 11:28:14 +0300

    Fix tests

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -9,24 +9,24 @@ beforeAll(() => {
   equal = fs.readFileSync(path.resolve(process.cwd(), '__fixtures__/deepDiff.txt'), 'utf8');
 });
 
-test('diff json files', () => {
-  const data = stylish(genDiff('__fixtures__/deepFile1.json', '__fixtures__/deepFile2.json'));
+test('stylish diff json files', () => {
+  const data = genDiff('__fixtures__/deepFile1.json', '__fixtures__/deepFile2.json', 'stylish');
   expect(data).toEqual(equal);
 });
 
-test('diff YML files', () => {
-  const data1 = stylish(genDiff('__fixtures__/deepFile1.yaml', '__fixtures__/deepFile2.yaml'));
-  const data2 = stylish(genDiff('__fixtures__/deepFile1.yml', '__fixtures__/deepFile2.yml'));
-  const data3 = stylish(genDiff('__fixtures__/deepFile1.yaml', '__fixtures__/deepFile2.yml'));
+test('stylish diff YML files', () => {
+  const data1 = genDiff('__fixtures__/deepFile1.yaml', '__fixtures__/deepFile2.yaml', 'stylish');
+  const data2 = genDiff('__fixtures__/deepFile1.yml', '__fixtures__/deepFile2.yml', 'stylish');
+  const data3 = genDiff('__fixtures__/deepFile1.yaml', '__fixtures__/deepFile2.yml', 'stylish');
 
   expect(data1).toEqual(equal);
   expect(data2).toEqual(equal);
   expect(data3).toEqual(equal);
 });
 
-test('diff YML and json files', () => {
-  const data1 = stylish(genDiff('__fixtures__/deepFile1.yaml', '__fixtures__/deepFile2.json'));
-  const data2 = stylish(genDiff('__fixtures__/deepFile1.yml', '__fixtures__/deepFile2.json'));
+test('stylish diff YML and json files', () => {
+  const data1 = genDiff('__fixtures__/deepFile1.yaml', '__fixtures__/deepFile2.json', 'stylish');
+  const data2 = genDiff('__fixtures__/deepFile1.yml', '__fixtures__/deepFile2.json', 'stylish');
 
   expect(data1).toEqual(equal);
   expect(data2).toEqual(equal);
