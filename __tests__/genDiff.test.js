a08ddebe0dde3b9b1ff622127c3b4c557b934943
Александр Невский <sashnevski@MacBook-Air-Aleksandr.local>
2024-09-02 16:41:48 +0300

    Add plain formatter

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -2,14 +2,45 @@ import fs from 'node:fs';
 import path from 'node:path';
 import genDiff from '../src/genDiff.js';
 
-let equal;
+describe('stylish diff', () => {
+  let equal;
+  const formatName = 'stylish';
 
-beforeAll(() => {
-  equal = fs.readFileSync(path.resolve(process.cwd(), '__fixtures__/deepDiff.txt'), 'utf8');
+  beforeAll(() => {
+    equal = fs.readFileSync(path.resolve(process.cwd(), '__fixtures__/deepDiff.txt'), 'utf8');
+  });
+
+  test('diff json files', () => {
+    const data = genDiff('__fixtures__/deepFile1.json', '__fixtures__/deepFile2.json', formatName);
+    expect(data).toEqual(equal);
+  });
+
+  test('diff YML files', () => {
+    const data1 = genDiff('__fixtures__/deepFile1.yaml', '__fixtures__/deepFile2.yaml', formatName);
+    const data2 = genDiff('__fixtures__/deepFile1.yml', '__fixtures__/deepFile2.yml', formatName);
+    const data3 = genDiff('__fixtures__/deepFile1.yaml', '__fixtures__/deepFile2.yml', formatName);
+
+    expect(data1).toEqual(equal);
+    expect(data2).toEqual(equal);
+    expect(data3).toEqual(equal);
+  });
+
+  test('diff YML and json files', () => {
+    const data1 = genDiff('__fixtures__/deepFile1.yaml', '__fixtures__/deepFile2.json', formatName);
+    const data2 = genDiff('__fixtures__/deepFile1.yml', '__fixtures__/deepFile2.json', formatName);
+
+    expect(data1).toEqual(equal);
+    expect(data2).toEqual(equal);
+  });
 });
 
-describe('stylish diff', () => {
-  const formatName = 'stylish';
+describe('plain diff', () => {
+  let equal;
+  const formatName = 'plain';
+
+  beforeAll(() => {
+    equal = fs.readFileSync(path.resolve(process.cwd(), '__fixtures__/plainDiff.txt'), 'utf8');
+  });
 
   test('diff json files', () => {
     const data = genDiff('__fixtures__/deepFile1.json', '__fixtures__/deepFile2.json', formatName);
--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -1,3 +1,50 @@
-const plain = () => '';
+import isPlainObject from 'lodash/isPlainObject.js';
+
+const formatValue = (value) => {
+  if (typeof value === 'string') {
+    return `'${value}'`;
+  }
+
+  if (isPlainObject(value)) {
+    return '[complex value]';
+  }
+
+  return value;
+};
+
+const plain = (tree) => {
+  const iter = (tree, path = '') => {
+    const lines = tree
+      .flatMap((node) => {
+        const { name, type, oldValue, newValue, children } = node;
+        const hasChildren = !!children;
+
+        const currentPath = path ? `${path}.${name}` : name;
+
+        if (hasChildren) {
+          return iter(children, currentPath);
+        }
+
+        switch (type) {
+          case 'unchanged':
+            return '';
+          case 'added':
+            return `Property '${currentPath}' was added with value: ${formatValue(newValue)}`;
+          case 'removed':
+            return `Property '${currentPath}' was removed`;
+          case 'updated':
+            return `Property '${currentPath}' was updated. From ${formatValue(
+              oldValue
+            )} to ${formatValue(newValue)}`;
+          default:
+            throw new Error('Type is not defined');
+        }
+      })
+      .filter((line) => line !== '');
+
+    return lines.join('\n');
+  };
+  return iter(tree);
+};
 
 export default plain;
