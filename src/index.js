6d967d434606e099bc7c27e4436ab57fc6d80916
Александр Невский <sashnevski@MacBook-Air-Aleksandr.local>
2024-09-12 13:00:03 +0300

    Test

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ const app = () => {
     .description('Compares two configuration files and shows a difference.')
     .version('1.0.0')
     .option('-f, --format [type]', 'output format', 'stylish')
+    .option('--colors', 'Enable colored output')
     .arguments('<filepath1> <filepath2>')
     .action((filepath1, filepath2, option) => {
       console.log(genDiff(filepath1, filepath2, option.format));
