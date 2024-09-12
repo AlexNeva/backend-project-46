8d9d27ab99b37f8d31d6b5e25475088f47bb3249
Александр Невский <sashnevski@MacBook-Air-Aleksandr.local>
2024-09-12 16:58:26 +0300

    Fix entry point

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,5 +1,16 @@
 #!/usr/bin/env node
 
-import app from '../src/index.js';
+import { program } from 'commander';
+import genDiff from '../src/index.js';
 
-app();
+program
+  .name('gendiff')
+  .description('Compares two configuration files and shows a difference.')
+  .version('1.0.0')
+  .option('-f, --format [type]', 'output format', 'stylish')
+  .arguments('<filepath1> <filepath2>')
+  .action((filepath1, filepath2, option) => {
+    console.log(genDiff(filepath1, filepath2, option.format));
+  });
+
+program.parse();
--- src/index.js
+++ src/index.js
@@ -1,21 +1,55 @@
-import { program } from 'commander';
-import genDiff from './genDiff.js';
+import path from 'node:path';
+import fs from 'node:fs';
+import sortBy from 'lodash/sortBy.js';
+import has from 'lodash/has.js';
+import isPlainObject from 'lodash/isPlainObject.js';
+import getParser from './parsers.js';
+import getFormatter from './formatters/index.js';
 
-const app = () => {
-  program
-    .name('gendiff')
-    .description('Compares two configuration files and shows a difference.')
-    .version('1.0.0')
-    .option('-f, --format [type]', 'output format', 'stylish')
-    .option('--colors', '')
-    .option('--runInBand', '')
-    .option('--bail', '')
-    .arguments('<filepath1> <filepath2>')
-    .action((filepath1, filepath2, option) => {
-      console.log(genDiff(filepath1, filepath2, option.format));
-    });
+const getPath = (filepath) => path.resolve(process.cwd(), filepath);
 
-  program.parse();
+const genDiff = (filepath1, filepath2, formatName) => {
+  const path1 = getPath(filepath1);
+  const path2 = getPath(filepath2);
+
+  const parseredFile1 = getParser(path.extname(path1))(fs.readFileSync(path1, 'utf8'));
+  const parseredFile2 = getParser(path.extname(path2))(fs.readFileSync(path2, 'utf8'));
+
+  const iter = (obj1, obj2) => {
+    const allKeys = new Set([...Object.keys(obj1), ...Object.keys(obj2)]);
+    const allSorteredKeys = sortBy([...allKeys]);
+
+    return allSorteredKeys.reduce((acc, key) => {
+      if (
+        has(obj1, key) &&
+        has(obj2, key) &&
+        isPlainObject(obj1[key]) &&
+        isPlainObject(obj2[key])
+      ) {
+        return [...acc, { name: key, children: iter(obj1[key], obj2[key]) }];
+      }
+
+      if (has(obj1, key) && !has(obj2, key)) {
+        return [...acc, { name: key, type: 'removed', oldValue: obj1[key] }];
+      }
+
+      if (!has(obj1, key) && has(obj2, key)) {
+        return [...acc, { name: key, type: 'added', newValue: obj2[key] }];
+      }
+
+      if (obj1[key] !== obj2[key]) {
+        return [...acc, { name: key, type: 'updated', oldValue: obj1[key], newValue: obj2[key] }];
+      }
+
+      return [...acc, { name: key, type: 'unchanged', oldValue: obj1[key] }];
+    }, []);
+  };
+
+  const diff = iter(parseredFile1, parseredFile2);
+
+  const formatter = getFormatter(formatName);
+
+  return formatter(diff);
 };
 
-export default app;
+export default genDiff;
