68ff459f43c054026593a0aea92ce7bbd27fdef2
Александр Невский <sashnevski@MacBook-Air-Aleksandr.local>
2024-09-12 11:49:35 +0300

    Test

--- eslint.config.js
+++ eslint.config.js
@@ -6,13 +6,13 @@ import { FlatCompat } from '@eslint/eslintrc';
 import pluginJs from '@eslint/js';
 import importPlugin from 'eslint-plugin-import';
 
-// // mimic CommonJS variables -- not needed if using CommonJS
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = path.dirname(__filename);
-// const compat = new FlatCompat({
-//   baseDirectory: __dirname,
-//   recommendedConfig: pluginJs.configs.recommended,
-// });
+// mimic CommonJS variables -- not needed if using CommonJS
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: pluginJs.configs.recommended,
+});
 
 export default [
   {
