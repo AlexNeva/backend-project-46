838b2d357777aa3652737cd4597e943d6394ec37
Александр Невский <sashnevski@MacBook-Air-Aleksandr.local>
2024-09-12 13:15:51 +0300

    Test

--- src/index.js
+++ src/index.js
@@ -5,11 +5,8 @@ const app = () => {
   program
     .name('gendiff')
     .description('Compares two configuration files and shows a difference.')
-    .version('12.0.0')
+    .version('1.0.0')
     .option('-f, --format [type]', 'output format', 'stylish')
-    .option('--colors', 'Enable colored output')
-    .option('--runInBand', '')
-    .option('--bail', '')
     .arguments('<filepath1> <filepath2>')
     .action((filepath1, filepath2, option) => {
       console.log(genDiff(filepath1, filepath2, option.format));
