3cfd66575d36b543995aed588e2f8fccfb6cadef
Александр Невский <sashnevski@MacBook-Air-Aleksandr.local>
2024-09-13 16:54:54 +0300

    Fix hexlet linter errors

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -1,8 +1,8 @@
 import fs from 'node:fs';
 import path, { dirname } from 'node:path';
-import genDiff from '../src/index.js';
 import { fileURLToPath } from 'node:url';
 import { expect, test } from '@jest/globals';
+import genDiff from '../src/index.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,12 +10,42 @@ const __dirname = dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
 const testCases = [
-  { ext1: 'yml', ext2: 'yml', format: undefined, expected: 'stylishDiff.txt' },
-  { ext1: 'yaml', ext2: 'yaml', format: undefined, expected: 'stylishDiff.txt' },
-  { ext1: 'json', ext2: 'json', format: undefined, expected: 'stylishDiff.txt' },
-  { ext1: 'yaml', ext2: 'yaml', format: 'stylish', expected: 'stylishDiff.txt' },
-  { ext1: 'yaml', ext2: 'yaml', format: 'plain', expected: 'plainDiff.txt' },
-  { ext1: 'yaml', ext2: 'yaml', format: 'json', expected: 'jsonDiff.txt' },
+  {
+    ext1: 'yml',
+    ext2: 'yml',
+    format: undefined,
+    expected: 'stylishDiff.txt',
+  },
+  {
+    ext1: 'yaml',
+    ext2: 'yaml',
+    format: undefined,
+    expected: 'stylishDiff.txt',
+  },
+  {
+    ext1: 'json',
+    ext2: 'json',
+    format: undefined,
+    expected: 'stylishDiff.txt',
+  },
+  {
+    ext1: 'yaml',
+    ext2: 'yaml',
+    format: 'stylish',
+    expected: 'stylishDiff.txt',
+  },
+  {
+    ext1: 'yaml',
+    ext2: 'yaml',
+    format: 'plain',
+    expected: 'plainDiff.txt',
+  },
+  {
+    ext1: 'yaml',
+    ext2: 'yaml',
+    format: 'json',
+    expected: 'jsonDiff.txt',
+  },
 ];
 
 test.each(testCases)(`getDiff($ext1, $ext2, $format)`, ({ ext1, ext2, format, expected }) => {
